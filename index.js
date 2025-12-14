02ba69ef319a4969b8faef8c8d484906ec29c3dd
Afnan-Afrid-Jamal <afnanafrid6556@gmail.com>
2025-12-14 23:18:18 +0600

    api created to get users data by role

--- index.js
+++ index.js
@@ -170,6 +170,22 @@ async function run() {
       }
     });
 
+    // Get all users data
+    app.get("/users", async (req, res) => {
+      try {
+        const users = await usersCollection.find().toArray();
+
+        if (users.length === 0) {
+          return res.status(404).json({ message: "No users found" });
+        }
+
+        res.status(200).json(users);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+    });
+
     // Add Scholarship
     app.post("/add-scholarship", async (req, res) => {
       try {
@@ -193,6 +209,26 @@ async function run() {
         res.status(500).json({ message: "Failed to add scholarship" });
       }
     });
+
+    // Get all users data by role
+    app.get("/users/role", async (req, res) => {
+      const { role } = req.query;
+      if (!role) {
+        return res.status(400).json({ message: "Role is required" });
+      }
+
+      try {
+        const result = await usersCollection.find({ role }).toArray();
+        res.status(200).json(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+    });
+
+    // Delete User
+
+    
 
     await client.db("admin").command({ ping: 1 });
     console.log(
